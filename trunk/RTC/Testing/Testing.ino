75320c0735ef36bc113d37b9ae28580ec059e329
cabbyboy7@gmail.com <cabbyboy7@gmail.com@3b02e49e-b582-799d-7dc0-d44e0d63c8ca>
2013-05-07 12:36:34 +0000

    SD manager aangemaakt

--- trunk/RTC/Testing/Testing.ino
+++ trunk/RTC/Testing/Testing.ino
@@ -4,15 +4,15 @@
 
 #include <Ethernet.h>
 #include <EthernetUdp.h>
-
+#include <SD.h>
 #include <Wire.h>
 
 NTP ntp;
 
 void setup(){
   Serial.begin(9600);
-  
-  
+  ntp.setup();
+  SD.begin(4);
 }
 
 
@@ -21,6 +21,7 @@ void loop(){
   Serial.println("Started");
 
   ntp.init();
+  ntp.PrintDateTime();
   Serial.println("Done");
   delay(1000);
 
