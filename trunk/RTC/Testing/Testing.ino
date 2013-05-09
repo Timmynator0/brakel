6beae42cfa9c7f6e8da076dc097adaa770a7f20e
cabbyboy7@gmail.com <cabbyboy7@gmail.com@3b02e49e-b582-799d-7dc0-d44e0d63c8ca>
2013-05-09 12:02:31 +0000

    SD Manager update

--- trunk/RTC/Testing/Testing.ino
+++ trunk/RTC/Testing/Testing.ino
@@ -1,28 +1,40 @@
-
 #include <SPI.h>
 #include <RTClib.h>
 
 #include <Ethernet.h>
 #include <EthernetUdp.h>
-#include <SD.h>
 #include <Wire.h>
+#include <SDManager.h>
 
 NTP ntp;
 
+SDManager manager;
+
+xbee_data data;
+char *test;
 void setup(){
   Serial.begin(9600);
+ manager.initSD();
+ 
   ntp.setup();
-  SD.begin(4);
+  data.day =ntp.getDate();
 }
 
 
 void loop(){
   
   Serial.println("Started");
+   
+  manager.writeToSD(&data);
 
-  ntp.init();
+//  ntp.init();
+ 
+  
   ntp.PrintDateTime();
   Serial.println("Done");
   delay(1000);
+  
+  
 
 }
+
