81e7536f4632194104ac71bf422653b41047c764
cabbyboy7@gmail.com <cabbyboy7@gmail.com@3b02e49e-b582-799d-7dc0-d44e0d63c8ca>
2013-05-14 16:43:16 +0000

    RTC get DateTime , SDManager Write from Struct

--- trunk/Libraries/SDManager/SDManager.h
+++ trunk/Libraries/SDManager/SDManager.h
@@ -5,24 +5,24 @@
 
 #include <SD.h>
 #include <string.h>
+#include <Types.h>
 
-typedef struct my_xbee {
-    char *day;
-    int temperatuur;
-    int licht;
-    int luchtvochtigheid;
-    int co2;
-    int nodeNr;
-}xbee_data;
+
+//typedef struct my_xbee {
+//    char *day;
+//    int temperatuur;
+//    int licht;
+//    int luchtvochtigheid;
+//    int co2;
+//    int nodeNr;
+//}xbee_data;
 
 class SDManager{
     
 public:
-    
     void initSD(void);
     void writeToSD(struct my_xbee *xbee);
     void readFromSD(void);
-    
 
 private:
     
