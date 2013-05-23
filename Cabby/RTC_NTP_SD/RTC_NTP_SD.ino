dd367268c4cc3d915acb21762a1ac687e1fa5c84
cabbyboy7@gmail.com <cabbyboy7@gmail.com@3b02e49e-b582-799d-7dc0-d44e0d63c8ca>
2013-05-23 10:32:15 +0000

    NTP RTC & SD Manager Test

--- Cabby/RTC_NTP_SD/RTC_NTP_SD.ino
+++ Cabby/RTC_NTP_SD/RTC_NTP_SD.ino
@@ -35,26 +35,26 @@ void loop(){
    data.nodeAddrLow = 5;
    data.nodeAddrHigh = 6;
    
-   //manager.removeFile("17052013.txt");
-//   manager.writeToSD(&data);
-//   Serial.println("Done Writing......");
+   manager.writeToSD(&data, true);
+   delay(1000);
+
+   Serial.println("Done Writing......");
   
- // ntp.PrintDateTime();
-  Serial.println("Start Reading......");
+//  ntp.PrintDateTime();
+//  Serial.println("Start Reading......");
 //  delay(1000);
 //  
-  manager.readFromSD(&data,"19052013.txt");
+  manager.readFromSD("offline.txt");
   Serial.println("Done Reading......");
-////  Serial.println(data.humidity);
-
-  Serial.println("Data is : ");
-  xbee_data* d;
-   d = manager.getData();
-   
-   for(int i = 0; i < manager.getDataSize();i++){
-    Serial.println(d[i].temperature);
-  
-   }
+//
+//  Serial.println("Data is : ");
+//  xbee_data* d;
+//   d = manager.getData();
+//   
+//   for(int i = 0; i < manager.getDataSize();i++){
+//    Serial.println(d[i].temperature);
+//  
+//   }
     delay(60L * 1000L);
 
 }
